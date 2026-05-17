272fa707e98ebc303d0278ae7d1d35df411f018d
kovidH333 <126075570+kovidH333@users.noreply.github.com>
2026-05-17 22:53:16 +0530

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.py
@@ -0,0 +1,38 @@
+# # 1. brute force
+# class Solution:
+#     def sortColors(self, nums: List[int]) -> None:
+#         """
+#         Do not return anything, modify nums in-place instead.
+#         """
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] > nums[j]:
+#                     temp = nums[i]
+#                     nums[i] = nums[j]
+#                     nums[j] = temp
+
+# 2.optimal
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        a = b = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                a += 1
+            elif nums[i] == 1:
+                b += 1
+
+        for i in range(len(nums)):
+            if a>0:
+                nums[i] = 0
+                a -= 1
+            elif b>0:
+                nums[i] = 1
+                b -= 1
+            else:
+                nums[i] = 2
+        
+        
+        
